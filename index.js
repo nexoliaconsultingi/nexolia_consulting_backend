d42fa536d36fdfceeca8c8d8df6f7a67f5a231cc
mohamed-wahbi <wahbisj@gmail.com>
2026-08-27 00:58:34 +0100

    ok for dash

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 //middlwaere :
 
 app.use(cors({
-  origin: [ "http://localhost:3000","http://localhost:3001","https://nexolia-consulting.com","https://api.nexolia-consulting.com","https://api.nexolia-consulting.com/chat/api","https://store.nexolia-consulting.com"],
+  origin: [ "http://localhost:3000","http://localhost:3001","https://nexolia-consulting.com","https://api.nexolia-consulting.com","https://api.nexolia-consulting.com/chat/api","https://store.nexolia-consulting.com","https://admin.nexolia-consulting.com","https://admin.nexolia-consulting.com/login"],
   methods: ['GET', 'POST','DELETE',"PUT","PATCH"],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
